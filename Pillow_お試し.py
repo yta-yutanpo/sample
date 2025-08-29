8309c29ae9cb845de8288e3ff12cad68e3f89639
yta-yutanpo <yuma-charo@docomo.ne.jp>
2025-08-29 18:49:48 +0900

    Pillow_お試し.pyの削除

--- Pillow_お試し.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-img = Image.new("RGB", (200, 300), (255, 255, 255))
-draw = ImageDraw.Draw(img)
-draw.rectangle([(0, 0), (199, 299)], outline=(0, 0, 0), width=5)
-font = ImageFont.truetype("seguisym.ttf", 128) #
-draw.text((10, 100), "\u2666", fill=(255, 0, 0), font=font)  # ♦
-draw.text((110, 100), "1", fill=(0, 0, 0), font=font)        # 1
-img.show()  # VS Codeなら画像ビューアで開く
